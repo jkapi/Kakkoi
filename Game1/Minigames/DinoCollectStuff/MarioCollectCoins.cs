1653f4443b6a5b5ebb106a763a79b35bc0c0084f
Laggbeer <36733506+Laggbeer@users.noreply.github.com>
2018-03-21 09:46:37 +0100

    update

---
+++ Game1/Minigames/DinoCollectStuff/MarioCollectCoins.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework.Input;
+using StrangerCade.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Minigames.MarioCollectCoins
+{
+    class DinoCollectStuff : Room
+    {
+        public DinoCollectStuff()
+
+        {
+
+        }
+
+
+
+        public override void Update()
+        {
+           if (Keyboard.Check(Keys.Left))
+            {
+
+            }
+           if (Keyboard.Check(Keys.Right))
+            {
+
+            }
+           if (Keyboard.Check(Keys.Up))
+            {
+
+            }
+           if (Keyboard.Check(Keys.Down))
+            {
+
+            }
+                    
+        }
+    }
+}
