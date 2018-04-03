aac71f93459a6d3f249f0dabd3cd279a860e8fdb
Laggbeer <36733506+Laggbeer@users.noreply.github.com>
2018-04-03 14:59:54 +0200

    meh

---
+++ Game1/Minigames/DinoCollectStuff/RozeDino.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using StrangerCade.Framework;
+
+namespace Game1.Minigames.DinoCollectStuff
+{
+    class RozeDino : GameObject
+    {
+        Sprite DinoSprite;
+        float xleft;
+        float xright;
+        float xspeed;
+
+        public RozeDino(Vector2 position, float xleft, float xright, float startspeed) : base(position)
+        {
+            this.xleft = xleft;
+            this.xright = xright;
+            this.xspeed = startspeed;
+        }
+
+        public override void Initialize()
+        {
+            DinoSprite = new Sprite(Room.Content.Load<Texture2D>("minigame/dinozooi/clown"));
+        }
+        public override void Update()
+        {
+            if (Position.X <= xleft || Position.X >= xright)
+            {
+                xspeed *= -1;
+            }
+            Position.X += xspeed;
+        }
+
+        public override void Draw()
+        {
+            if (xspeed > 0)
+                View.DrawSprite(DinoSprite, 0, Position);
+            else
+                View.DrawSprite(DinoSprite, 0, Position, null, 0, null, 0, null, SpriteEffects.FlipHorizontally);
+        }
+    }
+}
