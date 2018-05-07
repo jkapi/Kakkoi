946789f440e29684f8cf552c1cbc946e424feb71
Joey Weijters <385237@student.fontys.nl>
2018-05-07 11:54:10 +0200

    Added Main Menu Assets

--- Game1/Rooms/Menu.cs
+++ Game1/Rooms/Menu.cs
@@ -1,17 +1,23 @@
 ﻿using StrangerCade.Framework;
+using Game1.GameObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace Game1.Rooms
 {
     class Menu : Room
     {
+        private Texture2D KakoiLogo;
+
         public override void Initialize()
         {
-
+            MovingBackground.Init(Content);
+            KakoiLogo = Content.Load<Texture2D>("roomselect/menukakoilogo");
         }
 
         public override void Update()
@@ -21,7 +27,8 @@ namespace Game1.Rooms
 
         public override void Draw()
         {
-
+            MovingBackground.Draw(this);
+            View.DrawTexture(KakoiLogo, new Vector2(800, 540), null, 0, KakoiLogo.Bounds.Center.ToVector2());
         }
     }
 }
