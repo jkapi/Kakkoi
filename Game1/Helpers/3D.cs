05dcced9cba7bcb2e937d6141ba0f89533f17280
Joey Weijters <385237@student.fontys.nl>
2018-05-17 16:02:47 +0200

    3d test

---
+++ Game1/Helpers/3D.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Helpers
+{
+    class D3D
+    {
+        public RenderTarget2D RenderTarget;
+        public Color ClearColor;
+
+        public Vector3 CameraPosition;
+        public Vector3 CameraLookAtVector;
+        public Vector3 CameraUpVector;
+        public float CameraAspectRatio;
+        public float CameraFOV;
+        public float CameraNearClipPlane;
+        public float CameraFarClipPlane;
+
+        public bool EnableLighting;
+
+        private GraphicsDeviceManager g;
+        private GraphicsDevice gd;
+
+        public D3D(GraphicsDeviceManager graphics, RenderTarget2D target, Color? clearColor = null)
+        {
+            RenderTarget = target;
+            g = graphics;
+            gd = graphics.GraphicsDevice;
+            ClearColor = clearColor ?? Color.Transparent;
+
+            CameraPosition = new Vector3(target.Width / 2, target.Height / 2, -target.Height / 2);
+            CameraLookAtVector = new Vector3(target.Width / 2, target.Height / 2, 0);
+            CameraUpVector = Vector3.Down;
+
+            CameraAspectRatio = (float)target.Width / (float)target.Height;
+            CameraFOV = MathHelper.PiOver2;
+            CameraNearClipPlane = 1;
+            CameraFarClipPlane = target.Height + target.Width;
+
+            EnableLighting = false;
+        }
+
+        public void Begin()
+        {
+            gd.SetRenderTarget(RenderTarget);
+            gd.Clear(Color.Transparent);
+        }
+
+        public void End()
+        {
+            gd.SetRenderTarget(null);
+        }
+
+        public void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = projection;
+                }
+
+                mesh.Draw();
+            }
+        }
+
+        public void DrawTexture(Texture2D texture, Matrix transformation)
+        {
+            DrawTexture(texture, texture.Bounds.Size.ToVector2(), transformation);
+        }
+        public void DrawTexture(Texture2D texture, Vector2 size, Matrix transformation)
+        {
+            VertexPositionTexture[] verts = new VertexPositionTexture[6];
+            verts[0].Position = new Vector3(0, 0, 0);
+            verts[1].Position = new Vector3(0, size.Y, 0);
+            verts[2].Position = new Vector3(size.X, 0, 0);
+
+            verts[3].Position = verts[1].Position;
+            verts[4].Position = new Vector3(size.X, size.Y, 0);
+            verts[5].Position = verts[2].Position;
+            
+
+            verts[0].TextureCoordinate = new Vector2(0, 0);
+            verts[1].TextureCoordinate = new Vector2(0, 1);
+            verts[2].TextureCoordinate = new Vector2(1, 0);
+
+            verts[3].TextureCoordinate = verts[1].TextureCoordinate;
+            verts[4].TextureCoordinate = new Vector2(1, 1);
+            verts[5].TextureCoordinate = verts[2].TextureCoordinate;
+
+            BasicEffect effect = new BasicEffect(gd);
+            effect.View = Matrix.CreateLookAt(
+                CameraPosition, CameraLookAtVector, CameraUpVector);
+            effect.Projection = Matrix.CreatePerspectiveFieldOfView(
+                CameraFOV, CameraAspectRatio, CameraNearClipPlane, CameraFarClipPlane);
+
+            effect.World = transformation;
+
+            effect.TextureEnabled = true;
+            effect.Texture = texture;
+
+            foreach (var pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+
+                gd.DrawUserPrimitives(
+                            PrimitiveType.TriangleList,
+                    verts,
+                    0,
+                    2);
+            }
+
+        }
+    }
+}
--- Game1/Rooms/testminigame.cs
+++ Game1/Rooms/testminigame.cs
@@ -12,6 +12,7 @@ using Newtonsoft.Json.Linq;
 using System.Net;
 using System.IO;
 using System.Net.Http;
+using Game1.Helpers;
 
 namespace Game1.Rooms
 {
@@ -20,17 +21,22 @@ namespace Game1.Rooms
         SpriteFont Arial;
         Button btn;
         public Task<HttpResponseMessage> haalvraagop;
+        private Texture2D texture;
+        D3D d3d;
+
         public override void Initialize()
         {
+            d3d = new D3D(Graphics, new RenderTarget2D(GraphicsDevice, Graphics.PreferredBackBufferWidth, Graphics.PreferredBackBufferHeight));
             Arial = Content.Load<SpriteFont>("arial16");
-            //btn = new Button(new Vector2(20, 20), new Vector2(200, 30), Arial, "Ga naar room1");
-            //btn.OnClick += gomain;
-            //Objects.Add(btn);
-            //Objects.Add(new TextBoxAdvanced(new Vector2(20, 80), new Vector2(300, 200), Arial, "Vul iets in", false, "", true));
-            //Objects.Add(new CheckBox(new Vector2(400, 30), true) { Text = "Jeej" });
-            //Objects.Add(new CheckBox(new Vector2(400, 60), false));
-            //haalvraagop = new HttpClient().GetAsync("http://kakoi.ml/quiz.php");
             Objects.Add(new GameObjects.GameList(Vector2.Zero));
+            btn = new Button(new Vector2(20, 20), new Vector2(200, 30), Arial, "Ga naar room1");
+            btn.OnClick += gomain;
+            Objects.Add(btn);
+            Objects.Add(new TextBoxAdvanced(new Vector2(20, 80), new Vector2(300, 200), Arial, "Vul iets in", false, "", true));
+            Objects.Add(new CheckBox(new Vector2(400, 30), true) { Text = "Jeej" });
+            Objects.Add(new CheckBox(new Vector2(400, 60), false));
+            haalvraagop = new HttpClient().GetAsync("http://kakoi.ml/quiz.php");
+            texture = Content.Load<Texture2D>("roomselect/gamecovers");
         }
 
         private void gomain(object sender, EventArgs e)
@@ -38,23 +44,41 @@ namespace Game1.Rooms
             GotoRoom(typeof(Room1));
         }
 
+        public override void PreDraw()
+        {
+            DrawGround();
+        }
+
         public override void Draw()
         {
-        //    View.DrawText(Arial, "Boze tekst", new Vector2(20, 40));
-        //    string screenResolutions = "";
-        //    foreach(var mode in GraphicsAdapter.DefaultAdapter.SupportedDisplayModes)
-        //    {
-        //        if (mode.Width >= 800 && mode.Height >= 768)
-        //            screenResolutions += mode.Width + "x" + mode.Height + "\n";
-        //    }
-        //    if (haalvraagop.Status == TaskStatus.RanToCompletion)
-        //    {
-        //        var s = haalvraagop.Result.Content.ReadAsStringAsync();
-        //        s.Wait();
-        //        dynamic vraag = JObject.Parse(s.Result);
-        //        View.DrawText(Arial, vraag, new Vector2(800, 800));
-        //    }
-        //    View.DrawText(Arial, screenResolutions, new Vector2(600, 10));
+            View.DrawText(Arial, "Boze tekst", new Vector2(20, 40));
+            string screenResolutions = "";
+            foreach (var mode in GraphicsAdapter.DefaultAdapter.SupportedDisplayModes)
+            {
+                if (mode.Width >= 800 && mode.Height >= 768)
+                    screenResolutions += mode.Width + "x" + mode.Height + "\n";
+            }
+            if (haalvraagop.Status == TaskStatus.RanToCompletion)
+            {
+                var s = haalvraagop.Result.Content.ReadAsStringAsync();
+                s.Wait();
+                dynamic vraag = JObject.Parse(s.Result);
+                View.DrawText(Arial, vraag, new Vector2(800, 800));
+            }
+            View.DrawText(Arial, screenResolutions, new Vector2(600, 10));
+        }
+
+        public override void PostDraw()
+        {
+            View.DrawRenderTarget(d3d.RenderTarget, new Vector2(0, 0));
+        }
+
+        void DrawGround()
+        {
+            d3d.Begin();
+            Matrix translate = Matrix.CreateRotationX(MathHelper.ToRadians(45)) *
+                                 Matrix.CreateTranslation(new Vector3(0, 150, 0));
+            d3d.DrawTexture(texture, new Vector2(1920, 720), translate);
         }
     }
 }
